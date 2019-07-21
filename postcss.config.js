94926d486d623f4a6ad863afd53db294e6352ff4
Gabe Dunn <gabe.dunn@shaw.ca>
2019-07-20 18:19:29 -0600

    add tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
     require('tailwindcss'),
+    require('postcss-import'),
+    require('postcss-url'),
     require('autoprefixer'),
     require('postcss-nested'),
     require('cssnano')
