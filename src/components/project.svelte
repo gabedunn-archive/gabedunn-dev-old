8c23d7816ca803c06b2a4eecee25aa5b1fcad1be
Gabe Dunn <gabe.dunn@shaw.ca>
2019-07-22 19:13:58 -0600

    add project component and update projects page

---
+++ src/components/project.svelte
@@ -0,0 +1,33 @@
+<div class="max-w-sm m-4 rounded overflow-hidden shadow-lg">
+  <a {href} target="_blank">
+    <img
+        class="w-full"
+        src="{image}"
+        alt="{name} project image"
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+    >
+  </a>
+  <div class="px-6 py-4">
+    {#if github}
+    <div class="font-bold text-xl mb-2"><a href="{href}" target="_blank">{name}</a> | <a href="https://github.com/{github}" target="_blank">github</a></div>
+    {:else}
+    <div class="font-bold text-xl mb-2"><a href="{href}" target="_blank">{name}</a></div>
+    {/if}
+    <p class="text-gray-700 text-base">{description}</p>
+  </div>
+  <div class="px-6 py-4">
+    {#each skills as skill}
+    <span class="inline-block bg-gray-200 rounded-full p-3 py-1 my-1 text-sm font-semibold text-gray-700 mr-2">{skill}</span>
+    {/each}
+  </div>
+</div>
+
+<script>
+    export let name
+    export let description
+    export let href
+    export let github = false
+    export let image
+    export let skills = []
+</script>
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -2,5 +2,67 @@
 	<title>projects</title>
 </svelte:head>
 
-<h1 class="title-2">projects</h1>
-<h2 class="subtitle-2">This is my 'projects' page. There's not much here.</h2>
+<script>
+    import Project from '../components/project.svelte'
+
+    const projects = [
+      {
+        name: 'gabedunn.dev',
+        description: 'This website.',
+        href: 'https://gabedunn.dev',
+        github: 'redxtech/gabedunn.dev',
+        image: 'https://i.imgur.com/a3USZLo.png',
+        skills: ['html', 'css', 'js', 'svelte', 'sapper', 'tailwind']
+      },
+      {
+        name: 'when.',
+        description: 'A simple website to show when the next episode of your favourite TV shows will air.',
+        href: 'https://when.redxte.ch',
+        github: 'redxtech/when',
+        image: 'https://i.imgur.com/CjOQSan.png',
+        skills: ['html', 'css', 'js', 'vue', 'APIs', 'OAuth2', 'fetch']
+      },
+      {
+        name: 'canmore-brewing',
+        description: 'A landing page and informative website for a local brewery.',
+        href: 'https://canmorebrewing.com',
+        image: 'https://i.imgur.com/tlZ4dsK.png',
+        skills: ['html', 'css', 'js', 'vue', 'APIs', 'google maps']
+      },
+      {
+        name: 'devmod',
+        description: 'A moderation bot for a discord server.',
+        href: 'https://devcord.com',
+        github: 'redxtech/devmod',
+        image: 'https://i.imgur.com/ltBwusB.png',
+        skills: ['js', 'node', 'discord.js', 'async']
+      },
+      {
+        name: 'devcord',
+        description: 'A discord community that brings together web developers of all experience levels.',
+        href: 'https://devcord.com',
+        github: 'devcord',
+        image: 'https://i.imgur.com/Ov3QJaB.png',
+        skills: ['discord', 'community']
+      }
+    ]
+</script>
+
+<div class="mb-12">
+    <h1 class="title-2">projects</h1>
+    <h2 class="subtitle-2">A list of some of my projects, with a link to the project page and github, if it is public.</h2>
+</div>
+
+<div class="flex flex-wrap items-stretch content-center justify-around">
+    {#each projects as project}
+    <Project
+        name="{project.name}"
+        description="{project.description}"
+        href="{project.href}"
+        github="{project.github}"
+        image="{project.image}"
+        skills="{project.skills}"
+    />
+    {/each}
+</div>
+
