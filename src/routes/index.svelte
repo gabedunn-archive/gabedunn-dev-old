dad454f548eb680ae46f604fd13aa8ef7bd93f9a
Gabe Dunn <gabe.dunn@shaw.ca>
2019-10-14 16:53:50 -0700

    change software engineer -> software developer and make experience years dyanamic

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script>
+    const yearsSince = (from, to) => Math.abs(Math.round((to.getTime() - from.getTime()) / (1000 * 60 * 60 * 24 * 365.25)))
+    const devYears = yearsSince(new Date('2015-10-15'), new Date())
+</script>
 <svelte:head>
 	<title>Gabe Dunn</title>
 	<meta property="og:title" content="Gabe Dunn." />
@@ -5,13 +9,13 @@
 
 <div>
     <h1 class="title">Gabe Dunn.</h1>
-    <h2 class="subtitle">Full-stack web developer & software engineer.</h2>
+    <h2 class="subtitle">Full-stack web & software developer.</h2>
 </div>
 
 <div class="mt-24 text-lg font-light text-justify">
     <p class="mt-4">
-        Hello! I am a full-stack web developer and software engineer based in Calgary, Alberta and I've been working with web technologies for
-        the past 4 years.
+        Hello! I am a full-stack web and software developer based in Calgary, Alberta and I've been working with web
+        technologies for the past {devYears} years.
     </p>
     <p class="my-4">
         If you are interested in working with me please reach out via any of the contacts listed below.
